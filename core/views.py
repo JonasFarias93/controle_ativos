dee4a45dc24f94e3764bfa8a735d45860c3ea072
JonasFarias93 <166467757+JonasFarias93@users.noreply.github.com>
2026-01-14 11:51:31 +0000

    Fix: recupera template lista_lojas e corrige view no core

--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from registro.models import ItemBipado # Importante para carregar os dados
+from registro.models import ItemBipado, Loja # 1. Adicione 'Loja' no import
 
 def home(request):
-    # Buscamos os itens para o Painel de Movimentações
     movimentacoes = ItemBipado.objects.select_related('registro', 'registro__loja', 'registro__projeto').all().order_by('-registro__data_registro')
     return render(request, 'core/home.html', {'movimentacoes': movimentacoes})
 
 def lista_lojas(request):
-    # Função temporária para evitar erro no link do menu
-    return render(request, 'core/home.html') # Pode apontar para home por enquanto
+    # 2. Busque as lojas do banco de dados antes de passar para o render
+    lojas = Loja.objects.all() 
+    return render(request, 'core/lista_lojas.html', {'lojas': lojas})
