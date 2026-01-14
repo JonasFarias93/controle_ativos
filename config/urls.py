25328e95b24ab93d11bc3be028a6ad8a532b9399
JonasFarias93 <jhonas.jr@hotmail.com>
2026-01-14 20:57:25 -0300

    Fix: Resolve NoReverseMatch e finaliza integração da Navbar com Ativos

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ativos/', include('controle_ativos.urls')),
     path('', include('core.urls')), # Isso já engloba tudo que está no core
     path('registro/', include('registro.urls')),
     path('projetos/', include('projetos.urls')),
--- controle_ativos/urls.py
+++ controle_ativos/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'controle_ativos'
 
 urlpatterns = [
-    path('', views.lista_ativos, name='lista'),
+    path('', views.lista_ativos, name='lista_ativos'),
     path('adicionar/', views.adicionar_ativo_chamado, name='adicionar_ativo_chamado'),
 ]
